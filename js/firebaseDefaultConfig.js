824092bf7442ea4caf085863414c3c3504f385d6
ADMIN <tramdactrieu@gmail.com>
2023-03-11 08:58:26 +0700

    add test

---
+++ js/firebaseDefaultConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBECKfjMFJ5Kml9FF1F2PVXGi8O1nsYAhs",
+    authDomain: "webdashboard-eecea.firebaseapp.com",
+    projectId: "webdashboard-eecea",
+    storageBucket: "webdashboard-eecea.appspot.com",
+    messagingSenderId: "347419719757",
+    appId: "1:347419719757:web:689dcd1c7cfb97bdeb8404",
+    measurementId: "G-B8CY71NLQE",
+    databaseURL: "webdashboard-eecea-default-rtdb.firebaseio.com"
+};
