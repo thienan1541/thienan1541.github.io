1ef4a935a7635e2019cdf9a742dfb8a88956e8b8
ADMIN <tramdactrieu@gmail.com>
2023-03-12 20:19:47 +0700

    firebase

---
+++ js/nav.js
@@ -0,0 +1,19 @@
+$(function () {
+    function logOut() {
+        document.cookie.split(";").forEach(function (c) { document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/"); });
+        window.location.assign('/login');
+    }
+    // Get reference to the div element
+    const myDiv = document.getElementById('navBar');
+
+    // Fetch the content of the file
+    fetch('../nav.html')
+        .then(response => response.text())
+        .then(html => {
+            // Set the content of the div element
+            myDiv.innerHTML = html;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+});
