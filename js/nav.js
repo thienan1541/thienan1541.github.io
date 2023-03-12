33bde2011f4cb6e09137b48409bae8ff31faa72d
ADMIN <tramdactrieu@gmail.com>
2023-03-12 21:36:49 +0700

    firebase

--- js/nav.js
+++ js/nav.js
@@ -3,6 +3,10 @@ $(function () {
         document.cookie.split(";").forEach(function (c) { document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/"); });
         window.location.assign('/login');
     }
+
+    function retentionTool() {
+        window.location.assign('/retentiontool');
+    }
     // Get reference to the div element
     const myDiv = document.getElementById('navBar');
 
